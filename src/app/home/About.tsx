8aeb97eb361caa91ab4f35bf4fa13dc4d3a8a536
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-11-08 17:01:21 +0530

    Add about.tsx

---
+++ src/app/home/About.tsx
@@ -0,0 +1,12 @@
+import X_Mark from "@/components/xMark";
+
+export default function AboutSection() {
+  return (
+    <div>
+      <div></div>
+      AboutSection Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Totam esse, rerum repudiandae officia quasi numquam, sit sunt animi sed ea
+      earum dicta optio, error quos voluptatem odit reiciendis ipsam! Esse?
+    </div>
+  );
+}
